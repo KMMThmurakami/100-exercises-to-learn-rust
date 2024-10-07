c3b26b2cc02bf32d7057a07de12925e96fb0a5f7
gravity <gravity.chiffon+tech@gmail.com>
2024-10-07 15:31:23 +0900

    learn 6-11

--- exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -1,6 +1,12 @@
 // TODO: Define a function named `squared` that raises all `i32`s within a slice to the power of 2.
 //  The slice should be modified in place.
 
+fn squared(num: &mut [u32]) {
+    for n in num.iter_mut() {
+        *n = *n * *n;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
